88ae6b7d1cbf49c1a99530ad0ba5c398ad3b7345
0309naritani-crypto <0309naritani@gmail.com>
2025-11-10 20:29:55 +0900

    fix: change port to 10000 for Render

--- server.py
+++ server.py
@@ -43,6 +43,7 @@ def close_all_positions():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
